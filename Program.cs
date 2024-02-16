ac50c7fc60a4b7e24c40ff5fe323407e6235b10b
jra <julien.gault.50@gmail.com>
2024-02-16 17:55:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Random dice = new();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
